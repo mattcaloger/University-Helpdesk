fb4dd4d75d714cd117851db8adb290494bede99e
Matthew Caloger <mattcaloger@gmail.com>
2021-02-16 19:45:08 -0500

    Remove references to ticket_status

--- DAOs/currentUser.php
+++ DAOs/currentUser.php
@@ -15,7 +15,7 @@
     function getUserOpenTickets() {
         $db = $GLOBALS['db'];
         $session_token = $_COOKIE['sessionToken'];
-        $statement = $db->prepare("SELECT * FROM sessions INNER JOIN tickets ON sessions.user_id = tickets.ticket_creator INNER JOIN ticket_status ON tickets.ticket_status = ticket_status.status_id WHERE `session_token`=:session_token;");
+        $statement = $db->prepare("SELECT * FROM sessions INNER JOIN tickets ON sessions.user_id = tickets.ticket_creator WHERE `session_token`=:session_token;");
         $statement->bindParam(":session_token", $session_token);
         $statement->execute();
         $result = $statement->fetchAll();
--- Repositories/CurrentUserRepository.php
+++ Repositories/CurrentUserRepository.php
@@ -22,7 +22,7 @@
             try {
             $db = Database::getConnection();
             $session_token = $_COOKIE['sessionToken'];
-            $statement = $db->prepare("SELECT * FROM sessions INNER JOIN tickets ON sessions.user_id = tickets.ticket_creator INNER JOIN ticket_status ON tickets.ticket_status = ticket_status.status_id WHERE `session_token`=:session_token AND NOT tickets.`ticket_is_complete`=1 ORDER BY tickets.`ticket_id`;");
+            $statement = $db->prepare("SELECT * FROM sessions INNER JOIN tickets ON sessions.user_id = tickets.ticket_creator WHERE `session_token`=:session_token AND NOT tickets.`ticket_is_complete`=1 ORDER BY tickets.`ticket_id`;");
             $statement->bindParam(":session_token", $session_token);
             $statement->execute();
             $result = $statement->fetchAll();
@@ -56,7 +56,7 @@
         public static function getUserOpenAssignedTickets() {
             $db = Database::getConnection();
             $session_token = $_COOKIE['sessionToken'];
-            $statement = $db->prepare("SELECT * FROM sessions INNER JOIN tickets ON sessions.user_id = tickets.ticket_technician_owner INNER JOIN ticket_status ON tickets.ticket_status = ticket_status.status_id WHERE `session_token`=:session_token AND `ticket_is_complete`=0;");
+            $statement = $db->prepare("SELECT * FROM sessions INNER JOIN tickets ON sessions.user_id = tickets.ticket_technician_owner WHERE `session_token`=:session_token AND `ticket_is_complete`=0;");
             $statement->bindParam(":session_token", $session_token);
             $statement->execute();
             $result = $statement->fetchAll(PDO::FETCH_OBJ);
